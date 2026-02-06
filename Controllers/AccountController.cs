1af98b034185cb187e9558519a3a271f4b4ccced
Ali mobinifar <mobinifaraali62@gmail.com>
2026-02-06 11:44:10 +0330

    Add bank transaction and account controller pure

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BankAccountManagmentSystemApi.Controllers
+{
+    [ApiController]
+    [Route("api/account")]
+    public class AccountController : ControllerBase
+    {
+        public decimal Balance { get; set; }
+        public AccountController()
+        {
+        }
+
+        public async Task<IActionResult> CreateAccount() 
+        {
+            return Ok("Account created successfully");
+        }
+
+        public async Task<IActionResult> UpdateAccount()
+        {
+            return Ok("Account updated");
+        }
+
+    }
+}
---
+++ Controllers/BankTransactionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BankAccountManagmentSystemApi.Controllers
+{
+    public class BankTransactionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
